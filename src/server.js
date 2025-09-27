cecdad09f6c7697f36784a7dbd7d708672b732b6
Pirlo <potatoguy1511@gmail.com>
2025-09-27 15:38:23 +0700

    fix import path for viewEngine

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import configViewEngine from './config/viewEngine';
+import configViewEngine from './config/viewEngine.js';
 import dotenv from 'dotenv';
 dotenv.config()
 const app = express();
